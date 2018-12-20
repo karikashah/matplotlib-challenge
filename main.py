549be5bbfc5cb61cf86d80a97bb23e1a5d8d7930
Karika Shah <karikashah@yahoo.com>
2018-12-20 12:44:13 -0600

    Added code related to Bubble Plot of Ride Sharing Data

--- main.py
+++ main.py
@@ -19,28 +19,52 @@ print(ride_city_merge.head(5))
 # Obtain the x and y coordinates for each of the three city types
 # y-axis: Average Fare Per City
 average_fare = ride_city_merge.groupby("city")["fare"].mean()
-print(average_fare)
-average_fare = pdk.DataFrame(average_fare).reset_index()
-print("---------------------------------------------")
-print(average_fare)
-##average_fare = average_fare.rename(columns = {'fare': 'avg_fare'})
+average_fare_df = pdk.DataFrame(average_fare).reset_index()
+average_fare_df = average_fare_df.rename(columns = {'fare': 'avg_fare'})
+
 # x-axis: Total Number of Rides Per City
-# total_rides = combined_data.groupby("city")["ride_id"].count()
-# total_rides = pd.DataFrame(total_rides).reset_index()
-# total_rides = total_rides.rename(columns = {'ride_id': 'total_rides'})
-# #Total Drivers
-# total_drivers = combined_data[["city", "driver_count"]].drop_duplicates("city")
-# #City Type
-# city_type = combined_data[["city", "type"]].drop_duplicates("city")
-# final_df = pd.merge(pd.merge(pd.merge(average_fare, total_rides, on="city"), 
-#                              total_drivers, on="city"), city_type, on="city")
+total_rides = ride_city_merge.groupby("city")["ride_id"].count()
+total_rides_df = pdk.DataFrame(total_rides).reset_index()
+total_rides_df = total_rides_df.rename(columns = {'ride_id': 'total_rides'})
+
+# calculate total Drivers
+total_drivers = ride_city_merge[["city", "driver_count"]].drop_duplicates("city")
+# get city Type
+city_type = ride_city_merge[["city", "type"]].drop_duplicates("city")
+
+final_df = pdk.merge(pdk.merge(pdk.merge(average_fare_df, total_rides_df, on="city"), 
+                             total_drivers, on="city"), city_type, on="city")
+
 # Build the scatter plots for each city types
+urban_group = final_df.loc[final_df['type'] == 'Urban']
+suburban_group = final_df.loc[final_df['type'] == 'Suburban']
+rural_group = final_df.loc[final_df['type'] == 'Rural']
 
 # Incorporate the other graph properties
+ax1 = urban_group.plot(kind='scatter',x='total_rides', y='avg_fare',
+                       color='lightcoral', s=final_df['driver_count']*5, label = 'Urban', 
+                       alpha = 0.5, edgecolor = "black", linewidths = 1)    
+ax2 = suburban_group.plot(kind='scatter', x='total_rides', y='avg_fare', 
+                          color='lightskyblue',s=final_df['driver_count']*5, label = 'Suburban', 
+                          alpha = 0.5, edgecolor = "black", linewidths = 1, ax=ax1)    
+ax3 = rural_group.plot(kind='scatter', x='total_rides', y='avg_fare', 
+                       color='gold', s=final_df['driver_count']*5, label = 'Rural', 
+                       alpha = 0.5, edgecolor = "black", linewidths = 1, ax=ax1)
+plt.title("Pyber Ride Sharing Data (2016)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.ylabel("Average Fare ($)")
+
+# Incorporate a text label regarding circle size  
+textstr = 'Note:\nCircle size correlates with driver count per city.'
+plt.xlim(0, 42)
+plt.ylim(19, 45)
+print(textstr)
+plt.text(43, 32, textstr, fontsize=10)
+plt.subplots_adjust(right=0.55)
 
 # Create a legend
-
-# Incorporate a text label regarding circle size
-
+plt.legend(title = 'City Types')
+plt.grid(True)
 # Save Figure
-
+plt.savefig("./data/Bubble_01_ride_share.png")
+plt.show()
